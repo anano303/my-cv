451f99886d088a47338ffe4dbd2b7a41c7137bd2
anano303 <124179773+anano303@users.noreply.github.com>
2024-05-04 13:23:01 +0400

    add close icon and function

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -1,39 +1,102 @@
 import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import { LanguageContext } from "../../../Hooks/LanguageContext.js";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TEXTS } from "../../../Hooks/Languages.js";
 import menu from "../../../assets/menu.png";
+import ToggleOn from "../../../assets/toggle on (2).png";
+import ToggleOff from "../../../assets/toggle off.png";
+import geo from "../../../assets/geo.png";
+import eng from "../../../assets/eng.png";
+import { ThemeContext } from "../../../Hooks/ThemeContext";
+import { THEME } from "../../../Hooks/ThemeColors";
+import close from "../../../assets/close.png";
 
 const Navbar = () => {
-  const { language } = useContext(LanguageContext);
+  const { language, setLanguage } = useContext(LanguageContext);
+  const [isOn, setIsOn] = useState(true);
+  const themeContext = useContext(ThemeContext);
   //   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   useEffect(() => {
     document.body.className = language;
   }, [language]);
+  const handleClick = () => {
+    setIsOn((prevState) => !prevState);
+    // Toggle between 'dark' and 'light' themes
+    themeContext.setTheme((prevTheme) =>
+      prevTheme === "dark" ? "light" : "dark"
+    );
+  };
+
+  const handleLangClick = () => {
+    const newLanguage = language === "ge" ? "en" : "ge";
+    setLanguage(newLanguage); // Use setLanguage from LanguageContext to update the language
+  };
 
   return (
     <div className="Navbar">
-      <img alt="menu" src={menu} className="menu" />
-      <ul className="ul">
-        <li>
-          {" "}
-          <Link to="/">{TEXTS[language].home}</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/about">{TEXTS[language].about}</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/portfolio">{TEXTS[language].portfolio}</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/contact">{TEXTS[language].contact}</Link>
-        </li>
-      </ul>
+      <div className="menu" onClick={toggleMenu}>
+        {isMenuOpen ? (
+          <img alt="close" src={close} />
+        ) : (
+          <img alt="menu" src={menu} />
+        )}
+      </div>
+      <div
+        className={`links ${isMenuOpen ? "open" : ""}${
+          isMenuOpen ? "" : "close"
+        }`}
+      >
+        <ul className="ul">
+          <li>
+            {" "}
+            <Link to="/" onClick={closeMenu}>
+              {TEXTS[language].home}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/about" onClick={closeMenu}>
+              {TEXTS[language].about}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/portfolio" onClick={closeMenu}>
+              {TEXTS[language].portfolio}
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contact" onClick={closeMenu}>
+              {TEXTS[language].contact}
+            </Link>
+          </li>
+          <div className="mobileIcons">
+            <img
+              className="toggle"
+              src={isOn ? ToggleOn : ToggleOff}
+              alt="toggle"
+              onClick={handleClick}
+            />
+            <img
+              className="lang"
+              src={language === "ge" ? geo : eng}
+              alt="lang"
+              onClick={handleLangClick}
+            />
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
